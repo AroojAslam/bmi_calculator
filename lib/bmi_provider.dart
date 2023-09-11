8183781c2f2b57c6231cfa5d3339eaec0e86c996
AroojAslam <101873027+AroojAslam@users.noreply.github.com>
2023-09-11 12:39:28 +0500

    update provider screen

--- lib/bmi_provider.dart
+++ lib/bmi_provider.dart
@@ -2,5 +2,45 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class BMIProvider with ChangeNotifier{
+  int height = 180;
+  bool result = false;
+  String? status;
+  double _bmi = 0;
+  int weight = 70;
+   double get bmi => _bmi;
+  double per=0;
+  Color color(){
+    if (bmi >= 40) {
+      return Colors.red.shade900;
+    } else if (bmi >= 35) {
+      return Colors.orange.shade900;
+    } else if (bmi >= 30) {
+      return Colors.orange.shade500;
+    } else if (bmi >= 25) {
+      return Colors.yellow.shade500;
+    } else if (bmi >= 18.5) {
+      return Colors.green.shade800;
+    } else {
+      return Colors.blue.shade800;
+    }
+  }
+  void calculateBMI() {
+    _bmi = weight / pow((height / 100), 2);
+    if (bmi >= 40) {
+      status = "Obesity class 3";
+    } else if (bmi >= 35) {
+      status = 'Obesity class 2';
+    } else if (bmi >= 30) {
+      status = 'Obesity class 1';
+    } else if (bmi >= 25) {
+      status = 'Pre-Obesity';
+    } else if (bmi >= 18.5) {
+      status = 'Normal weight';
+    } else {
+      status = 'Underweight';
+    }
+     per= _bmi.roundToDouble()/140;
+    notifyListeners();
+  }
 
 }
--- lib/main_screen/home_screen.dart
+++ lib/main_screen/home_screen.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:kdgaugeview/kdgaugeview.dart';
 import 'package:provider/provider.dart';
-
+import 'package:percent_indicator/percent_indicator.dart';
 import '../bmi_provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,146 +12,115 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  bool result = false;
-  int age = 50;
-  String? status;
-  double bmi = 0;
-  int weight = 70;
-  int height = 180;
-  void calculateBMI() {
-    setState(() {
-      bmi = weight / pow((height / 100), 2);
-      if (bmi >= 40) {
-        status = "Obesity class 3";
-      } else if (bmi >= 35) {
-        status = 'Obesity class 2';
-      } else if (bmi >= 30) {
-        status = 'Obesity class 1';
-      } else if (bmi >= 25) {
-        status = 'Pre-Obesity';
-      } else if (bmi >= 18.5) {
-        status = 'Normal weight';
-      } else {
-        status = 'Underweight';
-      }
-    });
-  }
-
-  final speedNotifier = ValueNotifier<double>(10);
-  final key = GlobalKey<KdGaugeViewState>();
   @override
   Widget build(BuildContext context) {
     final bmiProvider = Provider.of<BMIProvider>(context,listen: false);
     return Scaffold(
+      appBar: AppBar(title: Text('BMI Calculator',style:TextStyle(color: Theme.of(context).primaryColor) )),
       body: SafeArea(
         child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: 60),
-               Text('Select your height and Weight',style: TextStyle(color: Theme.of(context).primaryColor,
-                   fontSize: 20,
-               fontWeight: FontWeight.w600)),
-              SizedBox(height: 20),
-              Card(
-                margin: EdgeInsets.all(20),
-                elevation: 2,
-                child: Column(
-                  children: [
-                    const SizedBox(height: 15),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Text(
-                          'Height : $height cm',style: TextStyle(color: Theme.of(context).primaryColor)
-                        ),
-                      ),
-                    ),
-                    Slider(
-                      value: height.toDouble(),
-                      min: 120,
-                      max: 220,
-                      onChanged: (double newValue) {
-                        setState(() {
-                          height = newValue.round();
-                        });
-                      },
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Text(
-                          'Weight : $weight KG',style: TextStyle(color: Theme.of(context).primaryColor)
-                        ),
-                      ),
-                    ),
-                    Slider(
-                      value: weight.toDouble(),
-                      min: 20,
-                      max: 200,
-                      onChanged: (double newValue) {
-                        setState(() {
-                          weight = newValue.round();
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      'Your BMI',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w600,color: Theme.of(context).primaryColor),
-                    ),
-                    const SizedBox(height: 20),
-                    Container(
-                      width: 230,
-                      height: 230,
-                      padding: EdgeInsets.all(10),
-                      child: KdGaugeView(
-                        unitOfMeasurement: ' ',
-                        unitOfMeasurementTextStyle: TextStyle(
-                            fontSize: 25,
-                            color: Theme.of(context).primaryColor),
-                        speedTextStyle: TextStyle(
-                            fontSize: 35,
-                            color: Theme.of(context).primaryColor),
-                        minSpeed: 0.0,
-                        maxSpeed: 90.0,
-                        speed: bmi.sign,
-                        animate: true,
-                        alertSpeedArray: [18.5, 25.0, 30.0, 35.0, 40, 90],
-                        alertColorArray: [
-                          Colors.blue.shade400,
-                          Colors.green.shade600,
-                          Colors.yellow,
-                          Colors.orange.shade400,
-                          Colors.orange.shade600,
-                          Colors.red.shade900
-                        ],
-                        duration: Duration(seconds: 3),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                  ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Consumer<BMIProvider>(builder: (context, value, child) {
+                    return Text(
+                        'Height : ${value.height} cm',style: TextStyle(color: Theme.of(context).primaryColor,fontWeight: FontWeight.w500,fontSize: 16)
+                    );
+                  },),
                 ),
               ),
-              result
-                  ? Text(status.toString(),
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w400,color: Theme.of(context).primaryColor))
-                  : Text(''),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    result = true;
-                  });
-                  calculateBMI();
-                },
-                child: Text('Calculate BMI'),
+              Consumer<BMIProvider>(builder: (context, value, child) {
+                return  Slider(
+                  value: value.height.toDouble(),
+                  min: 120,
+                  max: 220,
+                  onChanged: (newValue) {
+                    setState(() {
+                      value.height = newValue.toInt();
+                    });
+                  },
+                );
+              },),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Consumer<BMIProvider>(builder: (context, value, child) {
+                      return Text(
+                          'Weight : ${value.weight} KG',style: TextStyle(color: Theme.of(context).primaryColor,fontWeight: FontWeight.w500,fontSize: 16)
+                      );
+                    },)
+                ),
               ),
+              Consumer<BMIProvider>(builder: (context, value, child) {
+                return Slider(
+                  value: value.weight.toDouble(),
+                  min: 20,
+                  max: 200,
+                  onChanged: (double newValue) {
+                    setState(() {
+                      value.weight = newValue.round();
+                    });
+                  },
+                );
+              },),
+              const SizedBox(height: 10),
+              Text(
+                'Your BMI',
+                style:
+                TextStyle(fontSize: 18, fontWeight: FontWeight.w600,color: Theme.of(context).primaryColor),
+              ),
+              const SizedBox(height: 20),
+             Card(
+               elevation: 3,
+               margin: EdgeInsets.symmetric(horizontal: 20),
+               child: SizedBox(
+                 width:MediaQuery.of(context).size.width,
+                 child: Column(
+                   children: [
+                     const SizedBox(height: 20),
+                     Consumer<BMIProvider>(builder: (context, value, child) {
+                       return  CircularPercentIndicator(
+                         progressColor: value.color(),
+                         radius: 100,
+                         lineWidth: 10,
+                         circularStrokeCap: CircularStrokeCap.round,
+                         percent: value.per,
+                         center: Text(value.bmi.roundToDouble().toString(),style: TextStyle(
+                             fontSize: 35,color: value.color()),
+                         ),
+                       );
+                     }),
+
+                     const SizedBox(height: 20),
+                     Consumer<BMIProvider>(builder: (context, value, child) {
+                       return  value.result
+                           ? Text(value.status.toString(),
+                           style:
+                           TextStyle(fontSize: 20, fontWeight: FontWeight.w600,color: value.color()))
+                           : Text('');
+                     },),
+                     const SizedBox(height: 20),
+                   ],
+                 ),
+               ) ,
+             ),
+              const SizedBox(height: 50),
+             Consumer<BMIProvider>(builder: (context, value, child) {
+               return  ElevatedButton(
+                 onPressed: () {
+                   value.calculateBMI();
+                   value.result= true;
+                 },
+                 child: Text('Calculate BMI'),
+               );
+             },),
             ],
           ),
         ),
